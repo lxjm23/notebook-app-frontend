af60b221c9c272bc445788d619e5f5d08c422ba4
John Lopez <John Lopez>
2025-08-13 15:51:48 -0400

    url fixed

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000/"
-const API = process.env.REACT_APP_API_BASE || 'http://localhost:3001';
+// export const BACKEND_URL = "http://localhost:3000/"
+const BACKEND_URL = process.env.REACT_APP_API_BASE || 'http://localhost:3000';
