d92b7189c781b76d2d5a6a4aea02e50395dcaf0f
John Lopez <John Lopez>
2025-08-13 20:07:56 -0400

    some fix

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-import { apiUrl, API_BASE } from "../api";
+import { apiUrl, API_BASE, fetchWithRetry } from "../api";
 
 function App() {
 
